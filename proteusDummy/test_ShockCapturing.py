8f626a2287b1682532c5ffa604fdc002f13ead97
Chris Kees <christopher.e.kees@usace.army.mil>
2011-04-30 22:27:01 -0500

    added another test

--- proteusDummy/test_ShockCapturing.py
+++ proteusDummy/test_ShockCapturing.py
@@ -15,3 +15,11 @@ def testDummmy2():
     cd = ConstantDiffusion_SC(coefficients=C(),nd=3,shockCapturingFactor=0.25,lag=True)
     print "hello test"
     assert(True)
+
+def testDummmy3():
+    class C:
+        def __init__(self):
+            self.nc = 1
+    cd = ConstantDiffusion_SC(coefficients=C(),nd=3,shockCapturingFactor=0.25,lag=True)
+    print "hello test"
+    assert(True)
